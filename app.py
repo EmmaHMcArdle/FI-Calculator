bc0132fd56c6355326d78219212bdf4cb18ab93e
coolerstuff10 <emmahmcardle@gmail.com>
2021-01-25 12:08:01 -0500

    Added in sessions and connected calculations to flask app

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask.templating import render_template
 from flask_wtf import FlaskForm
-from wtforms import FloatField, SelectField, SubmitField, DecimalField, validators
+from wtforms import SelectField, SubmitField, DecimalField, validators
 from wtforms.validators import DataRequired
 from config import secret_key
+import calculations 
 
 app = Flask(__name__)
 
@@ -14,41 +15,54 @@ app.config['SECRET_KEY'] = secret_key
 def index():
     return render_template("index.html")
 
-@app.route("/calculator", methods=['GET', 'POST'])
-def calculator():
-    form = CalcForm()
-    if request.method=="POST":
-        a = form.financialGoal.data
-        p = form.invested.data
-        r = form.annualRate.data
-        n = form.compoundFreq.data
-        saving = form.oneTimeInvestment.data
-
-        print(a)
-        return redirect('/')
-    else:
-        return render_template("calculator.html", form=form)
-
 class CalcForm(FlaskForm):
-    financialGoal = FloatField('Financial Goal', validators=[DataRequired()])
-    invested = FloatField('Invested', validators=[DataRequired()])
-    annualRate = FloatField("Annual Rate %", validators=[DataRequired()])
+    financialGoal = DecimalField('Financial Goal', validators=[DataRequired()])
+    invested = DecimalField('Invested', validators=[DataRequired()])
+    annualRate = DecimalField("Annual Rate %", validators=[DataRequired()])
     # Do I need data required if automatic default?
     compoundFreq = SelectField('Compound Frequency', choices=[(365, 'Daily (365/year)'), 
                                 (360, 'Daily (360/year)'), (52, 'Weekly (52/year)'), 
                                 (26, 'Bi-Weekly (26/year)'), (24, 'Semi-Monthly (24/year)'), 
                                 (12, 'Monthly (12/year)'), (6, 'Bi-Monthly (6/year)'),
                                 (4, 'Quarterly (4/year)'), (2, 'Semi-Annually (2/year)'), (1, 'Annually (1/year)')])
-    oneTimeInvestment = FloatField('One-Time Investment')
-    continuousInvestment = FloatField("Continuous Investment")
+    oneTimeInvestment = DecimalField('One-Time Investment')
+    continuousInvestment = DecimalField("Continuous Investment")
     investmentFreq = SelectField("Investment Frequency", choices=[(365, 'Daily (365/year)'), 
                                 (360, 'Daily (360/year)'), (52, 'Weekly (52/year)'), 
                                 (26, 'Bi-Weekly (26/year)'), (24, 'Semi-Monthly (24/year)'), 
                                 (12, 'Monthly (12/year)'), (6, 'Bi-Monthly (6/year)'),
                                 (4, 'Quarterly (4/year)'), (2, 'Semi-Annually (2/year)'), (1, 'Annually (1/year)')])
-    annRate = DecimalField("Annual Rate", [validators.NumberRange(min=0, max=10)])
     submit = SubmitField("Calculate Time Saved")
 
+@app.route("/calculator", methods=['GET', 'POST'])
+def calculator():
+    form = CalcForm()
+    if request.method=="POST":
+        a = form.financialGoal.data
+        session['Goal'] = a
+        p = form.invested.data
+        session['Principal'] = p
+        r = form.annualRate.data
+        session['Annual Rate'] = r
+        n = form.compoundFreq.data
+        session['Compound Frequency'] = n
+        oneTimeSaving = form.oneTimeInvestment.data
+        session['One Time Savings'] = oneTimeSaving
+        contSaving = form.continuousInvestment.data
+        session['Continuous Savings'] = contSaving
+        freq = form.investmentFreq.data
+        session['Investment Frequency'] = freq
+        # if saving != None:
+            
+        return redirect(url_for('results'))
+    else:
+        return render_template("calculator.html", form=form)
+
+@app.route("/results")
+def results():
+    a = session.get('Goal', None)
+
+    return render_template("results.html")
 
 
 if __name__ == '__main__':
---
+++ calculations.py
@@ -0,0 +1,102 @@
+import math
+
+a = 1000000.0 #float(input("Enter in your fire number "))
+p = 50000.0 #float(input("Enter in the money you have invested "))
+r = 0.07 #float(input("Enter in your estimate rate of return "))
+n = 12 # assuming monthly compound interest
+oneTimeSavings = 1500.0
+repeatSavings = 650 #amount saved continuously
+#For continuous
+daily = float(1.0/365.0)
+# Compounding monthly and incrementing daily
+#100 a month / 365
+
+daysInMonth = 30.42
+
+def originalTimeline(a, p, n, r):
+    return (math.log(a/p)) / (n*(math.log(1 + (r/n))))
+
+def continuousPaymentTimeline(p, r, n, saving, a):
+    print("This is if you save $" + str(saving) + " a month")
+    days = 0
+    curA = 0.0
+    while curA < a:
+        curA = p*(1.0 + r/n)**(n*daily)
+        p = curA+(saving/daysInMonth)
+        days += 1
+    return days
+
+
+print("Originally calculated time: " + str(originalTimeline(a, p, n, r)))
+
+titles = ["month", "week", "day", "hour"]
+intervals = [12, 4.345, 7, 24]
+
+def daysToYears(time):
+    print(str(int(time)) + " years")
+    for i in range(4):
+        # t is a float, while int(t) is a integer so it automatically removes anything after decimal
+        time = ((time - int(time)) * intervals[i])
+        if int(time) > 0:
+            if int(time) > 1:
+                print(str(int(time)) + " " + titles[i] + "s")
+            else:
+                print(str(int(time)) + " " + titles[i])
+
+# def daysToYears(days):
+#     months = weeks = years = 0
+#     while ((days - 365) > 0):
+#         years += 1
+#         days -= 365
+#     print(str(years) + " years")
+#     while((days - daysInMonth) > 0):
+#         months += 1
+#         days -= daysInMonth
+#     print(str(months) + " months")
+#     while((days - 7) > 0):
+#         weeks += 1
+#         days -= 7
+#     print(str(weeks) + " weeks")
+#     print(str(int(days)) + " days")
+#     print("")
+        
+
+def calculate_diff(originTime, afterTime):
+    newT = originTime - afterTime
+    print(newT)
+    if int(newT) != 0:
+        print(str(int(newT)) + " years")
+    for i in range(4):
+        newT = (newT - int(newT)) * intervals[i]
+        if int(newT) > 0:
+            if int(newT) > 1:
+                print(str(int(newT)) + " " + titles[i] + "s")
+            else:
+                print(str(int(newT)) + " " + titles[i])
+
+# daysToYears(time)
+
+
+print("")
+print("This is how long until FIRE if you save amount:")
+p += repeatSavings
+afterTime = (math.log(a/p)) / (n*(math.log(1 + (r/n))))
+
+print("After calculated time: " + str(afterTime))
+daysToYears(afterTime)
+print("")
+print("You saved this amount of your time:")
+# calculate_diff(t, afterTime)
+
+# For continuous Payments
+days = continuousPaymentTimeline(p, r, n, 0, a)
+daysToYears(days)
+
+days = continuousPaymentTimeline(p, r, n, repeatSavings, a)
+daysToYears(days)
+
+days = continuousPaymentTimeline(p, r, n, repeatSavings+1500, a)
+daysToYears(days)
+
+days = continuousPaymentTimeline(p, 0.08, n, repeatSavings+1500, a)
+daysToYears(days)
