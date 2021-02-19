51b8e848f4726a050d609d0f794b41ea0532e81a
coolerstuff10 <emmahmcardle@gmail.com>
2021-02-19 15:46:06 -0500

    Remove virtual env

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ class MyFloatField(FloatField):
     def process_formdata(self, valuelist):
         if valuelist:
             try:
-                self.data = float(valuelist[0].replace(',', '.'))
+                self.data = float(valuelist[0].strip('$').replace(',', '.'))
             except ValueError:
                 self.data = None
                 raise ValueError(self.gettext('Not a valid float value'))
